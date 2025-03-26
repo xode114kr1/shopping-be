6cea4d1cd932a31a7349f8f79d754337dc3a72fa
신윤호 <xode114kr1@naver.com>
2025-03-26 19:45:12 +0900

    feat : Product 모델 생성

---
+++ models/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ProductSchema = Schema(
+  // UserSchema 정의
+  {
+    sku: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    category: { type: Array, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    stock: { type: Object, require: true },
+    status: { type: String, default: "active" },
+    isDeleted: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+ProductSchema.methods.toJSON = function () {
+  // userSchema를 프론트로 보낼 때 데이터를 필터함
+  const obj = this._doc;
+  delete obj.__v;
+  delete obj.updateAt;
+  delete obj.createAt;
+  return obj;
+};
+
+const Product = mongoose.model("Product", ProductSchema); // userSchema를 모델로 추출
+module.exports = Product;
