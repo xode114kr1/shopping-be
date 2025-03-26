65fa30cbdc7c099f5bd4eb7373cf359c1de921f7
신윤호 <xode114kr1@naver.com>
2025-03-26 19:22:24 +0900

    feat : app.js 기본 세팅 - express, mongoose, bodyParser, cors, dotenv 기본 세팅
    - mongoose 서버를 connect 후 5000번 포트에서 listen

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+require("dotenv").config(); // .env에 정의된 환경 변수를 process.env 에 로드
+app.use(cors()); // cors 사용 규칙
+
+// body-parser 사용 규칙
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("mongoose connected"))
+  .catch((e) => console.log("DB connection fail", e));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("server on");
+});
