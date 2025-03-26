a87cc47fdd701efc50d8a71f5e75458a5b939954
신윤호 <xode114kr1@naver.com>
2025-03-26 19:34:22 +0900

    feat : User 모델 생성 및 toJSON 메서드 추가 - email, password, name, level 필드 포함 - timestamps 활성화 - toJSON 메서드를 통해 불필요한 필드 제거

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = Schema(
+  // UserSchema 정의
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    level: { type: String, default: "customer" }, // 2type : customer, admin
+  },
+  { timestamps: true }
+);
+
+userSchema.methods.toJSON = function () {
+  // userSchema를 프론트로 보낼 때 데이터를 필터함
+  const obj = this._doc;
+  delete obj.password;
+  delete obj.__v;
+  delete obj.updateAt;
+  delete obj.createAt;
+  return obj;
+};
+
+const user = mongoose.model("User", userSchema); // userSchema를 모델로 추출
+module.exports = user;
